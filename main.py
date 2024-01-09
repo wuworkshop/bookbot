3892365834a2b36bcae7b45166c4a9276d57ad33
wuworkshop <wuworkshop@users.noreply.github.com>
2024-01-09 02:30:32 -0500

    Add functionality to print out the number of times each letter appears in the text file.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    count = get_word_count(text)
-    print(f"There are {count} words in the book.")
+    num_words = get_num_words(text)
+    letters_dict = get_letters_dict(text)
+    print(letters_dict)
 
 
 def get_book_text(path):
@@ -10,9 +11,19 @@ def get_book_text(path):
         return f.read()
 
 
-def get_word_count(text):
+def get_num_words(text):
     words_list = text.split()
     return len(words_list)
 
+
+def get_letters_dict(text):
+    text_lower = text.lower()
+    letters_dict = {}
+    for letter in text_lower:
+        if letter in letters_dict:
+            letters_dict[letter] += 1
+        else:
+            letters_dict[letter] = 1
+    return letters_dict
 
 main()
