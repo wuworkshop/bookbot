739d52d395caae282af75d15ae2b7113d2ecdc11
wuworkshop <wuworkshop@users.noreply.github.com>
2024-01-09 18:15:20 -0500

    Add functionality to print out a report that aggregates all the word and character data.

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    letters_dict = get_letters_dict(text)
-    print(letters_dict)
+    chars_dict = get_chars_dict(text)
+    print_report(book_path, num_words, chars_dict)
 
 
 def get_book_text(path):
@@ -16,14 +16,28 @@ def get_num_words(text):
     return len(words_list)
 
 
-def get_letters_dict(text):
+def get_chars_dict(text):
     text_lower = text.lower()
-    letters_dict = {}
-    for letter in text_lower:
-        if letter in letters_dict:
-            letters_dict[letter] += 1
+    chars_dict = {}
+    for char in text_lower:
+        if char in chars_dict:
+            chars_dict[char] += 1
         else:
-            letters_dict[letter] = 1
-    return letters_dict
+            chars_dict[char] = 1
+    return chars_dict
+
+
+def print_report(book_path, num_words, chars_dict):
+    # Convert chars_dict to a list of tuples and 
+    # sort in descending order by char count
+    chars_list = sorted(chars_dict.items(), key=lambda char: char[1], reverse=True)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"There are {num_words} words in the book.")
+    print("\n")
+    for char in chars_list:
+        if char[0].isalpha():
+            print(f'The "{char[0]}" character was found {char[1]} times.')
+    print("--- End report ---")
+
 
 main()
