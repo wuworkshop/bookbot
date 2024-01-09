c245b88f7262e944176512a837a5851f18eed1e9
wuworkshop <wuworkshop@users.noreply.github.com>
2024-01-09 02:04:30 -0500

    Add functionality to print out the number of words in the text file.

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    count = get_word_count(text)
+    print(f"There are {count} words in the book.")
+
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+
+def get_word_count(text):
+    words_list = text.split()
+    return len(words_list)
 
 
 main()
