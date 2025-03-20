1946571b5be93548df55f1e4feb763fd880d14da
wuworkshop <wuworkshop@users.noreply.github.com>
2025-03-20 03:45:29 -0400

    Move get_num_words function to stats.py.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import get_num_words
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
@@ -9,11 +11,6 @@ def main():
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-
-
-def get_num_words(text):
-    words_list = text.split()
-    return len(words_list)
 
 
 def get_chars_dict(text):
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_num_words(text):
+    words_list = text.split()
+    return len(words_list)
