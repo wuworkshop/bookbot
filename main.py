e807f2f68568962d5a261a88a5ec1e418bc1f98c
wuworkshop <wuworkshop@users.noreply.github.com>
2025-03-26 05:15:36 -0400

    Remove hard coded book path. Add functionality to pass in book path as a system argument.

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import sys
 from stats import get_num_words, get_chars_dict, sort_dict
 
+
 def main():
-    book_path = "books/frankenstein.txt"
+    try:
+        book_path = sys.argv[1]
+    except IndexError:
+        if len(sys.argv) != 2:
+            print("Usage: python3 main.py <path_to_book>")
+            sys.exit(1)
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
